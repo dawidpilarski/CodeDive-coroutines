d3b11feb100d3df59250ce9d4ffda3497576df6c
pilarski <3Ed4tf6yh78u!>
2019-11-18 20:54:20 +0100

    some improvements

---
+++ code-examples/retval-retvoid/void_task_notail.cpp
@@ -0,0 +1,9 @@
+task<void> display_text(string text){
+  // ...
+  // implicit co_return;
+}
+
+task<void> display_file(path file){
+  auto content = co_await read_file(file);
+  co_return display_text(content);
+}
